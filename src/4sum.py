001fabe676d6332c13c20bd08249f0cec1f613db
Sergiy Smirnov <snoff87@gmail.com>
2022-02-03 19:34:13 +0200

    pow pow yeah

---
+++ src/4sum.py
@@ -0,0 +1,24 @@
+import collections
+
+class Solution(object):
+    def fourSumCount(self, nums1, nums2, nums3, nums4):
+        ht = collections.defaultdict(int)
+
+        for n1 in nums1:
+            for n2 in nums2:
+                ht[n1 + n2] += 1
+        ans = 0
+        c=0
+        for n3 in nums3:
+            for n4 in nums4:
+                c = ht[-n3 - n4]
+                ans += c
+
+        return ans
+
+nums1 = [1,2]
+nums2 = [-2,-1]
+nums3 = [-1,2]
+nums4 = [0,2]
+sol = Solution()
+print(sol.fourSumCount(nums1, nums2, nums3, nums4))
