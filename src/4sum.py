4c8678aac5849d34508b191b53630dd01a74363b
Sergiy Smirnov <snoff87@gmail.com>
2022-02-04 21:37:11 +0200

    pow pow 2

--- src/4sum.py
+++ src/4sum.py
@@ -7,7 +7,7 @@ class Solution(object):
         for n1 in nums1:
             for n2 in nums2:
                 ht[n1 + n2] += 1
-
+                print(ht)
         ans = 0
         for n3 in nums3:
             for n4 in nums4:
