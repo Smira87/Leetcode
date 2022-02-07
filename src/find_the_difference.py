9b8352df43d695c53b64193cc40248cc119f26ae
Sergiy Smirnov <snoff87@gmail.com>
2022-02-07 17:59:21 +0200

    new task new version3

--- src/find_the_difference.py
+++ src/find_the_difference.py
@@ -8,8 +8,7 @@ class Solution(object):
         :rtype: str
         """
 
-        return (Counter(t) - Counter(s)).most_common()[0][0]
-
+        return (Counter(t) - Counter(s)).most_common(1)[0][0]
 
 sol = Solution()
 
