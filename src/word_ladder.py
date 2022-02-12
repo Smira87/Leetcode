df8fcf23cf5e64e81d26148d11a9f966d9322be4
Sergiy Smirnov <snoff87@gmail.com>
2022-02-12 16:47:03 +0200

    word ladder

--- src/word_ladder.py
+++ src/word_ladder.py
@@ -1,14 +1,9 @@
 from collections import defaultdict
 from collections import deque
 
+
 class Solution(object):
     def ladderLength(self, beginWord, endWord, wordList):
-        """
-        :type beginWord: str
-        :type endWord: str
-        :type wordList: List[str]
-        :rtype: int
-        """
         if endWord not in wordList:
             return 0
 
@@ -16,49 +11,28 @@ class Solution(object):
 
         return self.bfs(beginWord, endWord, wordHash)
 
-    def buildHash(self, wordList):
-        wordHash = defaultdict(list)
-        for word in wordList:
-            # Put * in every possible index and add up its prefix and suffix
-            for i in range(len(wordList[0])):
-                prefix = word[:i]
-                suffix = word[i + 1:]
-                wordHash[prefix + '*' + suffix].append(word)
-        return wordHash
-
     def bfs(self, beginWord, endWord, wordHash):
         visited = {}
         queue = deque([[beginWord, 1]])
         while queue:
-            popWord, level = queue.pop()
+            popWord, level = queue.popleft()
             visited[popWord] = True
 
             for i in range(len(popWord)):
-                prefix = popWord[:i]
-                suffix = popWord[i + 1:]
-                key = prefix + '*' + suffix
-
+                key = popWord[:i] + '*' + popWord[i + 1:]
                 for nextWord in wordHash[key]:
-
+                    # End point
                     if nextWord == endWord:
                         return level + 1
+                        # Append next word in queue
                     if nextWord not in visited:
                         queue.append([nextWord, level + 1])
                 wordHash[key] = []
-
         return 0
 
-
-
-
-sol = Solution()
-
-beginWord = "hot"
-endWord = "dog"
-wordList = ["hot","cog","dog","tot","hog","hop","pot","dot"]
-
-#print(sol.ladderLength(beginWord, endWord, wordList))
-
-hashWord = sol.buildHash(wordList)
-
-print(sol.ladderLength(beginWord, endWord, wordList))
+    def buildHash(self, wordList):
+        wordHash = defaultdict(list)
+        for word in wordList:
+            for i in range(len(wordList[0])):
+                wordHash[word[:i] + '*' + word[i + 1:]].append(word)
+        return wordHash
