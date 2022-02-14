df25511efd82f698167bd2c558df03c539c10f5d
Sergiy Smirnov <snoff87@gmail.com>
2022-02-15 00:37:40 +0200

    binary search 2

--- src/maximum_depth_of_binary_tree.py
+++ src/maximum_depth_of_binary_tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
          self.val = val
@@ -10,10 +12,18 @@ class Solution(object):
         if not root:
             return 0
 
-        left_count = self.maxDepth(root.right) + 1
-        right_count = self.maxDepth(root.left) + 1
+        queue = deque([[root, 1]])
 
-        return max(left_count, right_count)
+        while queue:
+
+            node, level = queue.popleft()
+
+            if node.left:
+                queue.append([node.left, level + 1])
+            if node.right:
+                queue.append([node.right, level + 1])
+
+        return level
 
 
 sol = Solution()
@@ -27,6 +37,5 @@ root.right = TreeNode(20)
 
 root.right.left = TreeNode(15)
 root.right.right = TreeNode(7)
-
 
 print(sol.maxDepth(root))
