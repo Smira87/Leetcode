5df1b9fcdb6316b5af8a90906e1ab9b163433b99
Serhii Smirnov <snoff87@gmail.com>
2023-07-16 13:38:13 +0300

    task141

---
+++ src/141. Linked List Cycle.py
@@ -0,0 +1,18 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        cur = head
+        while cur:
+            if cur.val == 'marked':
+                return True
+            cur.val = 'marked';
+            cur = cur.next
+        return False
