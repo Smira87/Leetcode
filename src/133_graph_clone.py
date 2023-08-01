6c065b61b561dc3ff1729b2ceef58e8262985fbf
Serhii Smirnov <snoff87@gmail.com>
2023-08-01 18:55:21 +0300

    133

--- src/133_graph_clone.py
+++ src/133_graph_clone.py
@@ -3,26 +3,15 @@ class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-
-
-
 class Solution:
-    def __init__(self):
-        self.visited = {}
-
     def cloneGraph(self, node: 'Node') -> 'Node':
-
-        if not node:
-            return
-
-        if node.val in self.visited:
-            return self.visited[node.val]
-
-        new_node = Node(node.val)
-
-        self.visited[node.val] = new_node
-
-        for neighbor in node.neighbors:
-            new_node.neighbors.append(self.cloneGraph(neighbor))
-
-        return new_node
+        oldToNew = {}
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+        return dfs(node) if node else None
