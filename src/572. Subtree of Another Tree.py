a5558d3b8d08c1019f4631018418d60dbb990331
Serhii Smirnov <snoff87@gmail.com>
2023-08-06 05:05:17 +0300

    572

---
+++ src/572. Subtree of Another Tree.py
@@ -0,0 +1,29 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+
+        def is_identical(node1, node2):
+            if not node1 and not node2:
+                return True
+            if not node1 or not node2:
+                return False
+            if node1.val != node2.val:
+                return False
+            return is_identical(node1.left, node2.left) and \
+                is_identical(node1.right, node2.right)
+
+        def dfs(node):
+            if not node:
+                return False
+            elif is_identical(node, subRoot):
+                return True
+            return dfs(node.left) or dfs(node.right)
+
+        return dfs(root)
