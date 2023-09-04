0e34b41194262d88d8128a325982ce2c4ef6e42c
Serhii Smirnov <snoff87@gmail.com>
2023-09-04 14:21:09 +0300

    199

---
+++ src/199. Binary Tree Right Side View.py
@@ -0,0 +1,41 @@
+import collections
+from typing import List, Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = collections.deque([root])
+
+        while q:
+            rightSide = None
+            qLen = len(q)
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
+        return res
+
+
+
+sol = Solution()
+node1 = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(3)
+node4 = TreeNode(4)
+node5 = TreeNode(5)
+
+node1.right = node3
+node1.left = node2
+node3.right = node4
+node2.right = node5
+
+print(sol.rightSideView(node1))
