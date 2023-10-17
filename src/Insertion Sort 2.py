f44adb3cb04ad6b60342b40790d4a02af18fd75e
Serhii Smirnov <snoff87@gmail.com>
2023-10-17 14:59:13 +0300

    InsertionSort2

---
+++ src/Insertion Sort 2.py
@@ -0,0 +1,12 @@
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while arr[j - 1] > arr[j] and j > 0:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
+            j -= 1
+
+arr = [2, 7, 5, 3, 6, 1]
+insertion_sort(arr)
+print(arr)
