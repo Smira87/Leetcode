0341bde9e389bd54edfc5b25fa42664e3ede3694
Serhii Smirnov <snoff87@gmail.com>
2023-11-30 16:02:04 +0200

    Factory Method Pattern

---
+++ src/Factory Method Pattern.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+class Vehicle(ABC):
+    @abstractmethod
+    def getType(self) -> str:
+        pass
+
+class Car(Vehicle):
+    def getType(self) -> str:
+        return "Car"
+
+class Bike(Vehicle):
+    def getType(self) -> str:
+        return "Bike"
+
+class Truck(Vehicle):
+    def getType(self) -> str:
+        return "Truck"
+
+class VehicleFactory(ABC):
+    @abstractmethod
+    def createVehicle(self) -> Vehicle:
+        pass
+
+class CarFactory(VehicleFactory):
+    # Write your code here
+    def createVehicle(self) -> Vehicle:
+        return Car()
+
+class BikeFactory(VehicleFactory):
+    # Write your code here
+    def createVehicle(self) -> Vehicle:
+        return Bike()
+
+class TruckFactory(VehicleFactory):
+    # Write your code here
+    def createVehicle(self) -> Vehicle:
+        return Truck()
+
+
+carFactory = CarFactory();
+truckFactory = TruckFactory();
+bikeFactory = BikeFactory();
+
+myCar = carFactory.createVehicle();
+myTruck = truckFactory.createVehicle();
+myBike = bikeFactory.createVehicle();
+
+print(myCar.getType())
+print(myTruck.getType())
+print(myBike.getType())
