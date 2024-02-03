446fc6f1a2e2297215aafedfdd90a3993886babd
Serhii Smirnov <snoff87@gmail.com>
2024-02-03 17:14:10 +0200

    704

---
+++ src/704. Binary Search.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l) // 2)
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
+sol = Solution()
+nums = [-1,0,3,5,9,12]
+target = 9
+print(sol.search(nums, target))
